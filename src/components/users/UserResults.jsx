79e3ea946335c9768c7dcd504f665d35f21392ce
chelz2 <golirida@hotmail.com>
2025-03-08 13:14:48 +0530

    users component - fetch users data github api

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import { data } from "react-router-dom";
+
 function UserResults() {
-  return <div>User Results</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  return (
+    <div>
+      {users.map((user) => (
+        <p>{user.login}</p>
+      ))}
+    </div>
+  );
 }
 
 export default UserResults;
