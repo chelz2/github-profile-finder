9a842792d676f1d459afa7f8fa7c9e47a486829d
chelz2 <golirida@hotmail.com>
2025-03-08 10:36:59 +0530

    page components - home, about, not found

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
     <>
       <Router>
         <div className="flex flex-col justify-between h-screen">
-          <Routes>
-            <Route path="/" element={<Navbar />}></Route>
-          </Routes>
-          <main className="container mx-auto px-3 pb-12">Content</main>
+          <Navbar />
+          <main className="container text-white mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+              <Route path="/notfound" element={<NotFound />}></Route>
+              <Route path="/*" element={<NotFound />}></Route>
+            </Routes>
+          </main>
           <Footer></Footer>
         </div>
       </Router>
---
+++ src/Pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">GitHub Profile Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        ReactJS frontend app to find github profile using Github API
+      </p>
+      <p className="text-lg">
+        made with love, <span text-white>Goli Rida.</span>
+      </p>
+    </div>
+  );
+}
+
+export default About;
