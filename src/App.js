0ade5f3004c32877509cdf67d633f19d0b5cf912
chelz2 <golirida@hotmail.com>
2025-03-15 18:33:54 +0530

    context hooks

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import Footer from "./components/layout/Footer";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import NotFound from "./Pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container text-white mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/about" element={<About />}></Route>
-              <Route path="/notfound" element={<NotFound />}></Route>
-              <Route path="/*" element={<NotFound />}></Route>
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </>
+    <GithubProvider>
+      <>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container text-white mx-auto px-3 pb-12">
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/notfound" element={<NotFound />}></Route>
+                <Route path="/*" element={<NotFound />}></Route>
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </>
+    </GithubProvider>
   );
 }
 
